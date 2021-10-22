fc8f2f5cdf3315ad02d9fc901d0336aaeed13514
Jack Taylor <JITB0824@users.noreply.github.com>
2021-10-22 13:45:25 -0700

    editing data sent

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const { getSystemErrorMap } = require('util')
 const app = express()
 const server = require('http').createServer(app)
 const WebSocket = require('ws')
+const d = new Date()
 
 const wss = new WebSocket.Server({ server: server })
+var x = new Array(300)
+var y = new Array(300)
+for (var i = 0; i = 300; i++) {
+    x[i] = i
+}
+for (var i = 0; i < 300; i++) {
+    y[i] = Math.random()
+}
+counter = 0
+graph_size = 100
+
+samples = 0
+tic = d.getTime()
+
+
+function get_graph_data() {
+    graph_to_send = JSON.stringify([x], [y])
+    console.log(JSON.stringify([x], [y]))
+    return graph_to_send
+}
 
 wss.on('connection', function connection(ws) {
     console.log('New client connected')
@@ -11,12 +33,8 @@ wss.on('connection', function connection(ws) {
 
     ws.on('message', function incoming(message) {
         console.log('recieved: %s', message)
-        ws.send('forwarding message to all clients!')
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message)
-            }
-        })
+        console.log(get_graph_data())
+        client.send(get_graph_data())
     })
 })
 
